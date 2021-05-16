6a71aa9ca7a3ac83c222b4b8397903598c0069e1
Luiz Gustavo <luizgustavorosa@outlook.com>
2021-05-16 15:08:27 -0300

    Codigo em produção

--- main.py
+++ main.py
@@ -8,16 +8,27 @@ Tela = pygame.display.set_mode([1080, 720])
 pygame.display.set_caption("PCA Game Sustentabilidade")
 
 #carregando sprites
-menu = pygame.image.load("assets/test-menu.png")
+
+menu = pygame.image.load("Layout/Telas/fundo_tela_inicial.png")
+ButtomStart = pygame.image.load("Layout/botoes/botão1.png")
+ButtomExit = pygame.image.load("Layout/botoes/botão2.png")
+ButtomCredits = pygame.image.load("Layout/botoes/botão3.png")
+
 
 #desenhando sprites
+
 Tela.blit(menu, (0, 0))
+Tela.blit(ButtomStart, (520, 260))
+Tela.blit(ButtomExit, (550, 420))
+Tela.blit(ButtomCredits, (550, 550))
+
 
 #colocando Musica
-pygame.mixer.music.load("sons/happy.mp3")
+
+pygame.mixer.music.load("sons/music.wav")
 pygame.mixer.music.play(-1)
 
-#Inicializando o pygame e criando a janela
+#Funcionalidades do jogo
 
 gameLoop = True
 
@@ -26,7 +37,17 @@ while gameLoop:
 
         if event.type == pygame.QUIT:
             gameLoop = False
-        if event.type == pygame.QUIT:
-            gameLoop = False
+#funcionalidade botões do menu
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ButtomExit = pygame.mouse.get_pos()
+            print(pygame.mouse.get_pos())
+            if 550 < ButtomExit[0] < (550 + 437) and 420 < ButtomExit[1] < (420 + 177):
+                gameLoop = False
+
+
 
     pygame.display.update()
+
+
+
