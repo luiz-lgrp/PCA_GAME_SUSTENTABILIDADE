44941fbe1000f98167ff9d2555c07addc51f8c97
yanbernardo <yan.bernardo24@hotmail.com>
2021-06-19 17:59:43 -0300

    Game 98% done
    Varias parada ai, preguiça de fazer a lista de updates

--- playing.py
+++ playing.py
@@ -34,7 +34,7 @@ def start_game():
     beepRight = pygame.mixer.Sound('sons/acertos.wav')
     beepRight.set_volume(0.4)
     beepWrong = pygame.mixer.Sound('sons/erros.wav')
-    beepWrong.set_volume(0.4)
+    beepWrong.set_volume(2)
     pygame.mixer.music.load("sons/happy.mp3")
     pygame.mixer.music.set_volume(0.05)
     pygame.mixer.music.play(-1)
